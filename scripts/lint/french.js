0e8544603e05e3a72a685233a37617ed8a50d6f0
maxlath <k@maxlath.eu>
2025-11-07 00:25:41 +0100

    lint: french: add comments and DRY

--- scripts/lint/french.js
+++ scripts/lint/french.js
@@ -11,16 +11,19 @@ function lintFrenchValue (value) {
     .replace(/oeuvre/g, 'œuvre')
     .replace(/Oeuvre/g, 'Œuvre')
 
+    // Écriture inclusive
     .replace(/auteur(e)/g, 'auteur·ice')
-    .replace(/auteur ou autrice/g, 'auteur·ice')
-    .replace(/un auteur/g, 'un·e auteur·ice')
-    .replace(/l['’]auteur([^·])/g, 'l’auteur·ice$1')
-    .replace(/l['’]auteur$/g, 'l’auteur·ice')
+    .replace(/(au|crea|utilisa)teur ou (au|crea|utilisa)trice/g, '$1teur·ice')
+    .replace(/un (au|crea|utilisa)teur/g, 'un·e $1teur·ice')
+    .replace(/l['’](au|crea|utilisa)teur([^·])/g, 'l’$1teur·ice$1')
+    .replace(/l['’](au|crea|utilisa)teur$/g, 'l’$1teur·ice')
 
-    .replace(/un utilisateur/g, 'un·e utilisateur·ice')
-    .replace(/l['’]utilisateur([^·])/g, 'l’utilisateur·ice$1')
-    .replace(/l['’]utilisateur$/g, 'l’utilisateur·ice')
-
+    // Add missing non-breaking spaces
     .replace(/([^\s])\s+([:;?!])/g, '$1 $2')
+
+    // Harmonize apostrophies
     .replace(/(\w)'(\w)/g, '$1’$2')
+
+    // Add missing "^" in "tâche"
+    .replace(/(t)ache(s?)/ig, '$1âche$2')
 }
