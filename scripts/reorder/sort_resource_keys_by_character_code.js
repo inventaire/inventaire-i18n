b2301b874d73dc229600440a7d3fa35115347d25
maxlath <k@maxlath.eu>
2026-02-19 10:27:11 +0100

    reorder: ignore case

--- scripts/reorder/sort_resource_keys_by_character_code.js
+++ scripts/reorder/sort_resource_keys_by_character_code.js
@@ -1,9 +1,12 @@
 export default function (resource) {
-  const entries = Object.entries(resource).sort(byCharacterCode)
+  const entries = Object.entries(resource).sort(byLowerCasedCharacterCode)
   return Object.fromEntries(entries)
 }
 
-function byCharacterCode ([ a ], [ b ]) {
+function byLowerCasedCharacterCode ([ a ], [ b ]) {
+  a = a.toLowerCase()
+  b = b.toLowerCase()
+
   for (let i = 0; i < a.length; i++) {
     if (b[i] == null) return 1
     if (a[i] !== b[i]) {
