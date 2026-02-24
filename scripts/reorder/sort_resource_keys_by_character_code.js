89f5471db6e17caac15e22b9e668ad6d2b34db1a
maxlath <k@maxlath.eu>
2026-02-24 20:48:19 +0100

    reorder: reverting to previous implementation to match Weblate ordering
    as seen when new translations are added to a file

--- scripts/reorder/sort_resource_keys_by_character_code.js
+++ scripts/reorder/sort_resource_keys_by_character_code.js
@@ -1,23 +1,9 @@
 export default function (resource) {
-  const entries = Object.entries(resource).sort(byLowerCasedCharacterCode)
+  const entries = Object.entries(resource).sort(byCharacterCode)
   return Object.fromEntries(entries)
 }
 
-const isPropertyId = key => /^P\d+$/.test(key)
-const parseNumericId = key => parseInt(key.substring(1))
-
-function byLowerCasedCharacterCode ([ a ], [ b ]) {
-  if (isPropertyId(a) && isPropertyId(b)) {
-    return parseNumericId(a) - parseNumericId(b)
-  } else if (isPropertyId(a)) {
-    return -1
-  } else if (isPropertyId(b)) {
-    return 1
-  }
-
-  a = a.toLowerCase()
-  b = b.toLowerCase()
-
+function byCharacterCode ([ a ], [ b ]) {
   for (let i = 0; i < a.length; i++) {
     if (b[i] == null) return 1
     if (a[i] !== b[i]) {
