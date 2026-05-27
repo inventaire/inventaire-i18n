20f1b97f8c8f802318c393cebd923759a932f047
maxlath <k@maxlath.eu>
2026-05-27 18:14:55 +0200

    get_lang_default_region: add zh_CN case

--- scripts/stats/lib/get_lang_default_region.js
+++ scripts/stats/lib/get_lang_default_region.js
@@ -1,10 +1,12 @@
-// Regions that aren't "#{lang}_#{lang.toUpperCase()}"
+// This language+region code seems to be based on ISO 3166-1 https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+// Regions that aren't `${lang}_${lang.toUpperCase()}`
 const specialCaseRegions = {
   da: 'da_DK',
   en: 'en_US',
   ja: 'ja_JP',
   no: 'nb_NO',
   sv: 'sv_SE',
+  zh: 'zh_CN',
 }
 
 export function getLangDefaultRegion (lang) {
